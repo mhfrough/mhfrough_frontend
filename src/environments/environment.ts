6a8eddd7704661a429fc2067ab3d42d9a946207a
Mohammad Hamza <mhfrough@yahoo.com>
2026-06-02 00:47:33 +0500

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,12 +2,12 @@ export const environment = {
     production: false,
     apiUrl: 'http://localhost:3023/api/v1',
     firebase: {
-        apiKey: 'YOUR_FIREBASE_API_KEY',
-        authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
-        projectId: 'YOUR_PROJECT_ID',
-        storageBucket: 'YOUR_PROJECT_ID.appspot.com',
-        messagingSenderId: '535081495411',
-        appId: 'YOUR_FIREBASE_APP_ID',
+        apiKey: "AIzaSyCIe9O51qNARv16vH2V8dGo537EX9PsexY",
+        authDomain: "mhfrough-4ffa1.firebaseapp.com",
+        projectId: "mhfrough",
+        storageBucket: "mhfrough.firebasestorage.app",
+        messagingSenderId: "535081495411",
+        appId: "1:535081495411:web:a3547bf5d01607670e7306",
         vapidKey: 'BCVFwVMD9CCgCb212pgvOLZcu-ALplvjcgPNz9VQSG9sO49cs2RktGsI4L0Z99z778eYQVP7NdVuuGZQMxszd2g',
     },
 };
