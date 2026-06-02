1118e5966ad7bcb1661136596013550f546bd38e
Mohammad Hamza <mhfrough@yahoo.com>
2026-06-02 23:38:42 +0500

    prod env

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://mhfrough-backend.onrender.com/api/v1',
+    firebase: {
+        apiKey: "AIzaSyCIe9O51qNARv16vH2V8dGo537EX9PsexY",
+        authDomain: "mhfrough-4ffa1.firebaseapp.com",
+        projectId: "mhfrough",
+        storageBucket: "mhfrough.firebasestorage.app",
+        messagingSenderId: "535081495411",
+        appId: "1:535081495411:web:a3547bf5d01607670e7306",
+        vapidKey: 'BCVFwVMD9CCgCb212pgvOLZcu-ALplvjcgPNz9VQSG9sO49cs2RktGsI4L0Z99z778eYQVP7NdVuuGZQMxszd2g',
+    },
+};
